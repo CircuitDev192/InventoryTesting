f578de6b5d03f355eca35e9c8ad6c116ca6e64fa
Matthew Ashmore <cpu.nut98@gmail.com>
2020-05-07 18:14:45 -0500

    Simple Inventory Added

---
+++ Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    [SerializeField] private Image Image;
+
+    private Item _item;
+    public Item Item 
+    {
+        get { return _item; }
+        set
+        {
+            _item = value;
+            if (_item == null)
+            {
+                Image.enabled = false;
+            }
+            else
+            {
+                Image.sprite = _item.Icon;
+                Image.enabled = true;
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (Image == null)
+        {
+            Image = GetComponent<Image>();
+        }
+    }
+}
