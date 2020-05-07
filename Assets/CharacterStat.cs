8421f1b9ec2ddffa69682a296f049e621af5f62a
Matthew Ashmore <cpu.nut98@gmail.com>
2020-05-07 17:10:29 -0500

    Base Implementation

---
+++ Assets/CharacterStat.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CharacterStat
+{
+    public float BaseValue;
+
+    public float Value
+    {
+        get
+        {
+            if (isDirty)
+            {
+                _value = CalculateFinalValue();
+                isDirty = false;
+            }
+            return _value;
+        }
+    }
+
+    private bool isDirty = true;
+    private float _value;
+
+    private readonly List<StatModifier> statModifiers;
+
+    public CharacterStat(float baseValue)
+    {
+        BaseValue = baseValue;
+
+        statModifiers = new List<StatModifier>();
+    }
+
+    public void AddModifier(StatModifier mod)
+    {
+        isDirty = true;
+        statModifiers.Add(mod);
+        statModifiers.Sort(CompareMoidiferOrder);
+    }
+
+    private int CompareMoidiferOrder(StatModifier a, StatModifier b)
+    {
+        if (a.Order < b.Order)
+            return -1;
+        else if (a.Order > b.Order)
+            return 1;
+        return 0;
+    }
+
+    public bool RemoveModifier(StatModifier mod)
+    {
+        isDirty = true;
+        return statModifiers.Remove(mod);
+    }
+
+    public float CalculateFinalValue()
+    {
+        float finalValue = BaseValue;
+
+        for (int i = 0; i < statModifiers.Count; i++)
+        {
+            StatModifier mod = statModifiers[i];
+
+            if (mod.Type == StatModType.Flat)
+            {
+                finalValue += mod.Value;
+            }
+            else if (mod.Type == StatModType.Percent)
+            {
+                finalValue *= 1 + mod.Value; //If mod is 10% increase, the value should be 0.1. This makes final value *= 1 + 0.1;
+            }
+        }
+
+        return (float)Math.Round(finalValue, 4);
+    }
+}
