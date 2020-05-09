dfa831038af1be04e829703736a08f9211d296bb
Matthew Ashmore <cpu.nut98@gmail.com>
2020-05-08 19:48:52 -0500

    Stat Tooltip Implementation

--- Assets/Scripts/StatDisplay.cs
+++ Assets/Scripts/StatDisplay.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using CircuitDev.CharacterStats;
 
-public class StatDisplay : MonoBehaviour
+public class StatDisplay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public Text NameText;
-    public Text ValueText;
+    private CharacterStat _stat;
+    public CharacterStat Stat 
+    {
+        get { return _stat; }
+        set
+        {
+            _stat = value;
+            UpdateStatValue();
+        }
+    }
+
+    private string _name;
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            _name = value;
+            nameText.text = _name.ToLower();
+        }
+    }
+
+    [SerializeField] private Text nameText;
+    [SerializeField] private Text valueText;
+    [SerializeField] private StatTooltip tooltip;
 
     private void OnValidate()
     {
         Text[] text = GetComponentsInChildren<Text>();
-        NameText = text[0];
-        ValueText = text[1];
+        nameText = text[0];
+        valueText = text[1];
+
+        if (tooltip == null)
+            tooltip = FindObjectOfType<StatTooltip>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltip.ShowToolTip(Stat, Name);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.HideToolTip();
+    }
+
+    public void UpdateStatValue()
+    {
+        valueText.text = _stat.Value.ToString();
     }
 }
---
+++ Assets/Scripts/StatTooltip.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using CircuitDev.CharacterStats;
+
+public class StatTooltip : MonoBehaviour
+{
+    [SerializeField] private Text StatNameText;
+    [SerializeField] private Text StatModifiersLabelText;
+    [SerializeField] private Text StatModifiersText;
+
+    private StringBuilder sb = new StringBuilder();
+
+    public void ShowToolTip(CharacterStat stat, string statName)
+    {
+        StatNameText.text = GetStatTopText(stat, statName);
+
+        StatModifiersText.text = GetStatModifersText(stat);
+
+        gameObject.SetActive(true);
+    }
+
+    public void HideToolTip()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private string GetStatTopText(CharacterStat stat, string statName)
+    {
+        sb.Length = 0;
+        sb.Append(statName);
+        sb.Append(" ");
+        sb.Append(stat.Value);
+
+        if (stat.Value != stat.BaseValue)
+        {
+            sb.Append(" (");
+            sb.Append(stat.BaseValue);
+
+            if (stat.Value > stat.BaseValue)
+                sb.Append("+");
+
+            sb.Append(System.Math.Round(stat.Value - stat.BaseValue, 4));
+            sb.Append(")");
+        }
+
+        return sb.ToString();
+    }
+
+    private string GetStatModifersText(CharacterStat stat)
+    {
+        sb.Length = 0;
+
+        foreach (StatModifier modifier in stat.StatModifiers)
+        {
+            if (sb.Length > 0)
+            {
+                sb.AppendLine();
+            }
+
+            if (modifier.Value > 0)
+                sb.Append("+");
+
+            if (modifier.Type == StatModType.Flat)
+            {
+                sb.Append(modifier.Value);
+            }
+            else
+            {
+                sb.Append(modifier.Value * 100);
+                sb.Append("%");
+            }
+
+            EquippableItem item = modifier.Source as EquippableItem;
+
+            if (item != null)
+            {
+                sb.Append(" ");
+                sb.Append(item.ItemName);
+            }
+            else
+            {
+                Debug.LogError("Modifier is not an equippable item!");
+            }
+        }
+
+        return sb.ToString();
+    }
+}
